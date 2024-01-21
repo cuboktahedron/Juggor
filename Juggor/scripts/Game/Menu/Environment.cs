edd3e9171233b626b63b1a0d848a57f68b7d9f27
cuboktahedron <cuboktahedron@gmail.com>
2024-01-21 10:32:29 +0900

    Rename constant to PascalCase

--- Juggor/scripts/Game/Menu/Environment.cs
+++ Juggor/scripts/Game/Menu/Environment.cs
@@ -3,6 +3,18 @@ namespace Juggor.Game.Menu;
 
 public partial class Environment : MenuButton
 {
+    private const int MenuIdTempo050 = 0;
+    private const int MenuIdTempo100 = 1;
+    private const int MenuIdTempo200 = 2;
+    private const int MenuIdTempo300 = 3;
+
+    private const int MenuIdGravity050 = 10;
+    private const int MenuIdGravity100 = 11;
+    private const int MenuIdGravity200 = 12;
+    private const int MenuIdGravity300 = 13;
+
+    private const int MenuIdMirror = 20;
+
     [Signal]
     public delegate void OnTempoChangedEventHandler(float tempo);
 
@@ -11,19 +23,6 @@ public partial class Environment : MenuButton
 
     [Signal]
     public delegate void OnMirrorChangedEventHandler(bool isMirror);
-
-    // TODO: Use pascal case
-    const int MENU_ID_TEMPO_0_5 = 0;
-    const int MENU_ID_TEMPO_1 = 1;
-    const int MENU_ID_TEMPO_2 = 2;
-    const int MENU_ID_TEMPO_3 = 3;
-
-    const int MENU_ID_GRAVITY_0_5 = 10;
-    const int MENU_ID_GRAVITY_1 = 11;
-    const int MENU_ID_GRAVITY_2 = 12;
-    const int MENU_ID_GRAVITY_3 = 13;
-
-    const int MENU_ID_MIRROR = 20;
 
     public override void _Ready()
     {
@@ -37,10 +36,10 @@ public partial class Environment : MenuButton
             Name = "Tempo"
         };
 
-        tempoMenu.AddItem("x 0.5", MENU_ID_TEMPO_0_5);
-        tempoMenu.AddItem("x 1", MENU_ID_TEMPO_1);
-        tempoMenu.AddItem("x 2", MENU_ID_TEMPO_2);
-        tempoMenu.AddItem("x 3", MENU_ID_TEMPO_3);
+        tempoMenu.AddItem("x 0.5", MenuIdTempo050);
+        tempoMenu.AddItem("x 1", MenuIdTempo100);
+        tempoMenu.AddItem("x 2", MenuIdTempo200);
+        tempoMenu.AddItem("x 3", MenuIdTempo300);
         popup.AddChild(tempoMenu);
 
         tempoMenu.IdPressed += OnItemPressed;
@@ -53,58 +52,58 @@ public partial class Environment : MenuButton
             Name = "Gravity"
         };
 
-        gravityMenu.AddItem("x 0.5", MENU_ID_GRAVITY_0_5);
-        gravityMenu.AddItem("x 1", MENU_ID_GRAVITY_1);
-        gravityMenu.AddItem("x 2", MENU_ID_GRAVITY_2);
-        gravityMenu.AddItem("x 3", MENU_ID_GRAVITY_3);
+        gravityMenu.AddItem("x 0.5", MenuIdGravity050);
+        gravityMenu.AddItem("x 1", MenuIdGravity100);
+        gravityMenu.AddItem("x 2", MenuIdGravity200);
+        gravityMenu.AddItem("x 3", MenuIdGravity300);
         popup.AddChild(gravityMenu);
 
         gravityMenu.IdPressed += OnItemPressed;
 
         // Mirror Menu
-        popup.AddCheckItem("Mirror", MENU_ID_MIRROR);
+        popup.AddCheckItem("Mirror", MenuIdMirror);
 
         popup.IdPressed += OnItemPressed;
     }
 
     private void OnItemPressed(long id)
     {
-        if (id == MENU_ID_TEMPO_0_5)
+        if (id == MenuIdTempo050)
         {
             EmitSignal(SignalName.OnTempoChanged, 0.5);
         }
-        else if (id == MENU_ID_TEMPO_1)
+        else if (id == MenuIdTempo100)
         {
             EmitSignal(SignalName.OnTempoChanged, 1.0);
         }
-        else if (id == MENU_ID_TEMPO_2)
+        else if (id == MenuIdTempo200)
         {
             EmitSignal(SignalName.OnTempoChanged, 2.0);
         }
-        else if (id == MENU_ID_TEMPO_3)
+        else if (id == MenuIdTempo300)
         {
             EmitSignal(SignalName.OnTempoChanged, 3.0);
         }
-        else if (id == MENU_ID_GRAVITY_0_5)
+        else if (id == MenuIdGravity050)
         {
             EmitSignal(SignalName.OnGravityChanged, 0.5);
         }
-        else if (id == MENU_ID_GRAVITY_1)
+        else if (id == MenuIdGravity100)
         {
             EmitSignal(SignalName.OnGravityChanged, 1.0);
         }
-        else if (id == MENU_ID_GRAVITY_2)
+        else if (id == MenuIdGravity200)
         {
             EmitSignal(SignalName.OnGravityChanged, 2.0);
         }
-        else if (id == MENU_ID_GRAVITY_3)
+        else if (id == MenuIdGravity300)
         {
             EmitSignal(SignalName.OnGravityChanged, 3.0);
         }
-        else if (id == MENU_ID_MIRROR)
+        else if (id == MenuIdMirror)
         {
             var popup = GetPopup();
-            var index = popup.GetItemIndex(MENU_ID_MIRROR);
+            var index = popup.GetItemIndex(MenuIdMirror);
             popup.ToggleItemChecked(index);
             bool isChecked = popup.IsItemChecked(index);
             EmitSignal(SignalName.OnMirrorChanged, isChecked);
